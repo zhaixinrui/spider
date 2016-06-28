d5ed5257a6b2b562e866b5b4dcffc5f22293dcf2
zhaixinrui <zhaixinrui@bogon.office>
2016-06-28 18:35:07 +0800

    百尊跳投注页

--- baizun.py
+++ baizun.py
@@ -13,6 +13,9 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import urllib
+import random
+
 from conf import conf
 
 class Site(object):
@@ -23,9 +26,30 @@ class Site(object):
         self.browser.implicitly_wait(10)
         self.conf = conf['sites'][self.siteName]
 
+    def getVerifyCode(self, imgUrl=''):
+        # api = 'http://api.shikexin.com/ws/api/verifyCodeRead?appKey=dda9e208e6255dd2da3e36fd6fab7a31&codetype=1004&filetype=2&imgurl=%s' % imgUrl
+        api = 'http://www.zun1777.com/app/member/verify/mkCode.php?_=%s%s' % (random.random(), ('%.3f' % time.time()).replace('.', ''))
+        f = urllib.urlopen(api)
+        spl = f.readline().split(';')
+        # self.browser.find_element_by_name('SS').send_keys(spl[0])
+        # self.browser.find_element_by_name('SR').send_keys(spl[1])
+        # self.browser.find_element_by_name('TS').send_keys(spl[2])
+        self.browser.execute_script('document.getElementsByName("SS").value = "%s";' % spl[0])
+        self.browser.execute_script('document.getElementsByName("SR").value = "%s";' % spl[1])
+        self.browser.execute_script('document.getElementsByName("TS").value = "%s";' % spl[2])
+        print spl
+
+        imgUrl = 'http://www.zun1777.com/tpl/commonFile/images/gdpic/macpic.php?SR=%s' % spl[1]
+        api = 'http://api.shikexin.com/ws/api/verifyCodeRead?appKey=dda9e208e6255dd2da3e36fd6fab7a31&codetype=1004&filetype=2&imgurl=%s' % imgUrl
+        f = urllib.urlopen(api)
+        ret = f.readline()
+        ret = json.loads(ret)
+        print ret
+        return ret['data']['verifyCode']
+
     def login(self):
         self.browser.get(self.conf['url'])
-        print self.browser.page_source
+        # print self.browser.page_source
 
         # 切到iframe
         self.browser.execute_script('document.getElementsByTagName("iframe")[0].id="iframe"')
@@ -50,39 +74,46 @@ class Site(object):
         btnLogin      = self.browser.find_element_by_class_name('btnLogin')
         inputUserName.send_keys(self.conf['username'])
         inputPasswd.send_keys(self.conf['passwd'])
+        inputRmNum.click()
+
         print '自动填充用户名密码'
+        img = WebDriverWait(self.browser, 20).until(EC.presence_of_element_located((By.ID, "vPic")))
+        vc = s.getVerifyCode()
+        inputRmNum.send_keys(vc)
+        btnLogin.click()
+        # imgUrl = img.get_attribute("src")
+        # print imgUrl
+        # # 等待验证码填写完成后登录
+        # while True:
+        #     v = inputRmNum.get_attribute("value")
+        #     if(len(v) == 4):
+        #         btnLogin.click()
+        #         break
+        #     time.sleep(0.1)
+        time.sleep(100000)
+        # # 登录成功后自动关掉提示框
+        # WebDriverWait(self.browser,10).until(EC.alert_is_present())
+        # self.browser.switch_to.alert.accept()
+        # WebDriverWait(self.browser,10).until(EC.alert_is_present())
+        # self.browser.switch_to.alert.accept()
+        # print '登录成功后自动关掉提示框'
 
-        # 等待验证码填写完成后登录
-        while True:
-            v = inputRmNum.get_attribute("value")
-            if(len(v) == 4):
-                btnLogin.click()
-                break
-            time.sleep(0.1)
+        # # 同意用户协议
+        # btns = self.browser.find_elements_by_class_name('za_button')
+        # btns[1].click()
+        # print '同意用户协议'
 
-        # 登录成功后自动关掉提示框
-        WebDriverWait(self.browser,10).until(EC.alert_is_present())
-        self.browser.switch_to.alert.accept()
-        WebDriverWait(self.browser,10).until(EC.alert_is_present())
-        self.browser.switch_to.alert.accept()
-        print '登录成功后自动关掉提示框'
-
-        # 同意用户协议
-        btns = self.browser.find_elements_by_class_name('za_button')
-        btns[1].click()
-        print '同意用户协议'
-
-        # 等待浮框加载后关掉
-        try:
-            element = WebDriverWait(self.browser, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "ui-dialog-titlebar-close"))
-            )
-            btnCloseDialog = self.browser.find_element_by_class_name('ui-dialog-titlebar-close')
-            time.sleep(2)
-            btnCloseDialog.click()
-            print '关掉支付提示浮层'
-        except Exception, e:
-            print '关闭支付提示浮层失败，未找到'
+        # # 等待浮框加载后关掉
+        # try:
+        #     element = WebDriverWait(self.browser, 10).until(
+        #         EC.presence_of_element_located((By.CLASS_NAME, "ui-dialog-titlebar-close"))
+        #     )
+        #     btnCloseDialog = self.browser.find_element_by_class_name('ui-dialog-titlebar-close')
+        #     time.sleep(2)
+        #     btnCloseDialog.click()
+        #     print '关掉支付提示浮层'
+        # except Exception, e:
+        #     print '关闭支付提示浮层失败，未找到'
 
     def touzhu(self):
         while True:
@@ -112,7 +143,7 @@ if __name__ == '__main__':
         s.login()
         s.touzhu()
         time.sleep(100000)
-    except Exception, e:
-        raise e
+    # except Exception, e:
+    #     raise e
     finally:
         s.clear()
